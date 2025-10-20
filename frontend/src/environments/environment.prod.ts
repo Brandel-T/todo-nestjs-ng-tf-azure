9178bc48c2cae480a65cfee0d9d781d8e169c56b
Brandel-T <tsagueubrandel@gmail.com>
2025-10-20 10:02:00 +0200

    fix: add missing HTTPS protocol to API URI in production environment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURI: 'todo-backend-abd5gyaqfvbfg5c2.westeurope-01.azurewebsites.net/api',
+  apiURI: 'https://todo-backend-abd5gyaqfvbfg5c2.westeurope-01.azurewebsites.net/api',
 };
